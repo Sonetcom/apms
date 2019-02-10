f99744c336ccac87b35f542c4e96b469f8af72fe
hibozzy18 <hknguruwe@gmail.com>
2019-02-10 15:11:16 +0100

    inital push

--- users/models.py
+++ users/models.py
@@ -5,21 +5,80 @@ class Person(models.Model):
     """ Person class"""
 
     # Personal Data 
+    user_id = models.IntegerField()
     first_name = models.CharField(max_length=70)
     second_name = models.CharField(max_length=70)
     surname_name = models.CharField(max_length=70)
     maiden_name = models.CharField(max_length=70)
+    sex = models.CharField()
     date_of_birth = models.DateTimeField()
     create_date = models.DateTimeField('date created', auto_created=True)
     academic_level = models.CharField(max_length=100)
+    profile = models.CharField(max_length=400)
 
     # contact details
     work_number = models.IntegerField()
     home_number = models.IntegerField()
     cell_number = models.IntegerField()
+    email = models.CharField()
+
+    def __init__(self):
 
 
+#===============================================================
+#   Student inherits from  users
+#===============================================================
+class Student(Person):
+    """docstring for ClassName"""
 
+    # Additional data 
+    stud_id = models.CharField()
+
+    #intrests must be done with a single word in the list 
+    #that we have to loop through the list
+    intrests = models.CharField(max_length=500)
+
+    # next of Keen
+    """
+    Next of keen is not  partof the user object, used just as fields 
+    but can be changed later to a model in te latest version.
+    The next keen values are prefixed by proceeding n_ to show that 
+    they are next of keen 
+    """
+    n_first_name = models.CharField(max_length=70)
+    n_surname_name = models.CharField(max_length=70)
+    n_email = models.CharField()
+
+    def __init__(self):
+        super(Student, self).__init__()
+        self.arg = arg
+
+
+#===============================================================
+#   Adminstrators inistitutional for inherits from  users
+#   I Adminstrators are institutional adminstrators
+#===============================================================
+class I_Adminstrator(Person):
+    """docstring for ClassName"""
+    
+    admin_id=models.CharField()
+    inst_id= models.CharField()
+
+    def __init__(self):
+        super(I_Adminstrator, self).__init__()
+        self.arg = arg
+      
+
+#===============================================================
+#   System adminstrators inistitutional for inherits from  users
+#.  S Adminstrators are system adminstrators
+#===============================================================
+class S_Adminstrator(Person):
+    """docstring for ClassName"""
+    def __init__(self):
+        super(Student, self).__init__()
+        self.arg = arg
+      
     def __str__(self):
         """Return resonable name"""
         var_name = self.first_name + self.surname_name
